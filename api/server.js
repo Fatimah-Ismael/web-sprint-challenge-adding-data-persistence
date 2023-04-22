526c038642c957d2f97459b7a63354ad1618e7fa
Fatimah Ismael <fatimahismael@yahoo.com>
2023-04-21 22:00:44 -0700

    dependencies, scripts, server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+
+server.use('*', (req, res)=>{
+    res.status(404).json({
+      message:'Sorry, not found'
+    })
+})
+/*
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+  
+});*/
+
+module.exports = server;
+
